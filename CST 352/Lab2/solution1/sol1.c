fff1a28c8bfe8a68ea1173b5ceaf2f2343070cea
Mineshopper <kmartshopper@gmail.com>
2015-04-26 01:07:49 -0700

    Final submission made for lab2.

--- CST 352/Lab2/solution1/sol1.c
+++ CST 352/Lab2/solution1/sol1.c
@@ -1,3 +1,12 @@
+/***********************************************************************
+* CST 352 Operating Systems - Lab 2 - Solution 1
+* Logan Wright
+*
+* Code listing from "Advanced Linux Programming," by CodeSourcery LLC
+* Copyright (C) 2001 by New Riders Publishing
+* See COPYRIGHT for license information.
+***********************************************************************/
+
 #include <time.h>
 #include <stdlib.h>
 #include <pthread.h>
